0f183f2c8e2c286b3b37294760f6b90e33c0543c
VolkCristina <kristina.volk@coderscrew.pl>
2022-07-20 16:00:09 +0200

    basic setup

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService{
+  constructor(private http: HttpClient) {
+
+  }
+
+  getAll() {
+    this.http.get('https://fakestoreapi.com/products/')
+  }
+}
